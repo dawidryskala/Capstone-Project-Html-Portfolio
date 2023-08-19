0f9a721d848b2fdffb543664ff7474710e81a353
dawidryskala <141305850+dawidryskala@users.noreply.github.com>
2023-08-19 15:44:14 +0200

    Create index.js
    Add in js alert explanation

---
+++ index.js
@@ -0,0 +1,4 @@
+document.querySelector(".explanation").addEventListener("click", function contact_button() {
+    alert("This feature will be implemented soon. Apologies for the inconvenience. For now, please contact me via GitHub.")
+}
+);
